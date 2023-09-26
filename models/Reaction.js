7cf53fb7c8b267dff369a50865bd4520ae1703a2
zeckenrode <zeckenrode9000@gmail.com>
2023-09-25 20:35:02 -0400

    Esablished the connection with mongoose/server, and made the models

---
+++ models/Reaction.js
@@ -0,0 +1,50 @@
+/* Reaction Schema
+
+    1. reactionId
+        - Use Mongoose's ObjectId datatype
+        - Default value is set to a new ObjectId
+    2. reactionBody
+        - String
+        - Required
+        - 280 max char
+    3. username
+        - String
+        - Required
+    4. createdAt
+        - Date
+        - Set defualt val to current timestamp
+        - Use getter method to format timestamp on query
+    
+Not a model, used as reaction field's subdocument in the Thought model 
+
+*/
+
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            defualt: () => new Date(),
+            get: (date) => {
+                return date.toLocaleString();
+            },
+        }
+        
+    }
+);
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -16,23 +16,43 @@
 
 Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 
-Reaction Schema
-
-    1. reactionId
-        - Use Mongoose's ObjectId datatype
-        - Default value is set to a new ObjectId
-    2. reactionBody
-        - String
-        - Required
-        - 280 max char
-    3. username
-        - String
-        - Required
-    4. createdAt
-        - Date
-        - Set defualt val to current timestamp
-        - Use getter method to format timestamp on query
-    
-Not a model, used as reaction field's subdocument in the Thought model
-
 */
+
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280,
+            min_length: 1,
+        },
+        createdAt: {
+            type: Date,
+            defualt: () => new Date(),
+            get: (date) => {
+                return date.toLocaleString();
+            },
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
